0d00924b4111ec4cd52ba7ed0ef366f46aae4352
beepe0 <douks.bs@gmail.com>
2022-03-03 16:00:47 +0100

    Update

--- CodeZadanie/CodeZadanie/CodeZadanie.cpp
+++ CodeZadanie/CodeZadanie/CodeZadanie.cpp
@@ -1,7 +1,30 @@
-﻿
-#include <iostream>
-
+﻿#include <iostream>
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    int countN;
+    cin >> countN;
+
+    int i = 0;
+
+    while (i < countN)
+    {
+        int miss = 0;
+        int curr = 2;
+        int i = 2;
+        for (;;)
+        {
+            i++;
+            if (curr % i == 0)
+            {
+                miss++;
+                if (miss < 3) break;
+            }
+            
+        }
+        cout << curr;
+        curr = curr + 1;
+        i++;
+    }
+
 }
