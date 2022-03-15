4d319c8f703b258ccda3fa005157bbfbb2e48afd
beepe0 <douks.bs@gmail.com>
2022-03-15 19:02:12 +0100

    The End

--- CodeZadanie/CodeZadanie/CodeZadanie.cpp
+++ CodeZadanie/CodeZadanie/CodeZadanie.cpp
@@ -1,35 +1,21 @@
 #include <iostream>
-#include <limits>
 using namespace std;
-
-
-
-
 
 int main()
 {
-    int arbuz;
-    int temp;
+    int countTime;
     int lp = 0;
-    int countl;
+    int countl = 0;
     int nextl = 2;
     cout << "Podaj ile chcesz wygenerować liczb: ";
-    cin >> arbuz;
-    
-    while(lp < arbuz)
+    cin >> countTime;
+
+    while(lp < countTime)
     {
-        for(int d = 2; d < nextl-1; d++)
-        {
-            if(nextl % d == 1)
-            {
-                countl++;
-                //cout << " " << nextl;
-            }
-            
-        }
-        if(countl > 2 || countl == 0)cout << " " << nextl;
+        for(int d = 2; d <= nextl - 1; d++)
+            if(nextl % d == 0){countl++;}
+        if(countl == 0){ cout << " " << nextl; lp++;}
         nextl++;
-        lp++;
+        countl = 0;
     }
-    
 }
