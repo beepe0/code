de2d436dbe332e37c554e88ef90aca95c61f75c7
Dmytro Shakhun <dshakhun501@technikumkreatywne.pl>
2022-04-20 12:39:27 +0200

    up

--- Algorytm Sprawdzanie Anagramów/Algorytm Sprawdzanie Anagramów/main.cpp
+++ Algorytm Sprawdzanie Anagramów/Algorytm Sprawdzanie Anagramów/main.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 
-bool isEquals(string word, string nextWord);
+bool isEquals(std::string word, std::string nextWord);
 int main()
 {
-    string word, nextWord;
-    cin >> word;
-    cin >> nextWord;
+    std::string word, nextWord;
+    std::cout << "Pierwsze słowo: " << std::endl;
+    std::cin >> word;
+    std::cout << "Drugie słowo: " << std::endl;
+    std::cin >> nextWord;
     if(isEquals(word, nextWord)) std::cout << "To slowo jest jednakowe: " << word << " == " << nextWord;
     else std::cout << "To slowo nie jest jednakowe: " << word << " != " << nextWord;
     return 0;
 }
 
-bool isEquals(string word, string nextWord)
+
+
+bool isEquals(std::string word, std::string nextWord)
 {
     int suma = 0;
     if((word.size() != nextWord.size())) return false;
