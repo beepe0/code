305a8287d746efa31db27abd6f14a4b2068ea38c
Dmytro Shakhun <dshakhun501@technikumkreatywne.pl>
2022-04-20 11:52:31 +0200

    #include <iostream>

---
+++ Algorytm Sprawdzanie Anagramów/Algorytm Sprawdzanie Anagramów/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+bool isEquals(string word, string nextWord);
+int main()
+{
+    string word, nextWord;
+    cin >> word;
+    cin >> nextWord;
+    if(isEquals(word, nextWord)) std::cout << "To slowo jest jednakowe: " << word << " == " << nextWord;
+    else std::cout << "To slowo nie jest jednakowe: " << word << " != " << nextWord;
+    return 0;
+}
+
+bool isEquals(string word, string nextWord)
+{
+    int suma = 0;
+    if((word.size() != nextWord.size())) return false;
+    for(int i = 0; i < word.size(); i++) suma += (int)word[i] - (int)nextWord[i];
+    if(suma == 0) return true; else return false;
+}
---
+++ Algorytm Sprawdzanie Anagramów/README.md
@@ -0,0 +1,1 @@
+# Algorytm Sprawdzanie Anagramów
